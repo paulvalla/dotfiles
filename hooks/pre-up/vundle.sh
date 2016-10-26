b68f04b26397130ac3b0a9388b89d6b8fe3111e0
Paul Valla <pvalla@thoughtworks.com>
2016-10-26 13:26:07 +0530

    Added vundle and vimrc

---
+++ hooks/pre-up/vundle.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+VUNDLE_VC_DIR=~/.vim/bundle/Vundle.vim
+
+if [ ! -d $VUNDLE_VC_DIR ]
+then
+    echo cloning vundle to $VUNDLE_VC_DIR 
+    git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_VC_DIR
+fi
+
